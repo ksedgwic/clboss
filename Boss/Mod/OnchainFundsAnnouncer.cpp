ccd784b057f0ddf6d6861fde31d4927558493514
Ken Sedgwick <ken@bonsai.com>
2025-12-17 17:08:00 -0800

    coroutines: convert OnchainFundsAnnouncer to C++20 coroutines

--- Boss/Mod/OnchainFundsAnnouncer.cpp
+++ Boss/Mod/OnchainFundsAnnouncer.cpp
@@ -8,6 +8,7 @@
 #include"Boss/concurrent.hpp"
 #include"Boss/log.hpp"
 #include"Ev/Io.hpp"
+#include"Ev/coroutine.hpp"
 #include"Jsmn/Object.hpp"
 #include"Json/Out.hpp"
 #include"S/Bus.hpp"
@@ -52,55 +53,63 @@ void OnchainFundsAnnouncer::start() {
 }
 
 Ev::Io<void> OnchainFundsAnnouncer::on_block() {
-	return Ev::lift().then([this]() {
-		return get_ignore_rr.execute(Msg::RequestGetOnchainIgnoreFlag{
-			nullptr
-		});
-	}).then([this](Msg::ResponseGetOnchainIgnoreFlag res) {
-		if (res.ignore)
-			return Boss::log( bus, Info
-					, "OnchainFundsAnnouncer: "
-					  "Ignoring onchain funds until "
-					  "%f seconds from now."
-					, res.seconds
-					);
-		return announce();
-	});
+	auto res = co_await get_ignore_rr.execute(
+		Msg::RequestGetOnchainIgnoreFlag{nullptr}
+	);
+	if (res.ignore) {
+		co_await Boss::log( bus, Info
+				  , "OnchainFundsAnnouncer: "
+				    "Ignoring onchain funds until "
+				    "%f seconds from now."
+				  , res.seconds
+				  );
+		co_return;
+	}
+	co_await announce();
 }
 
 Ev::Io<void> OnchainFundsAnnouncer::announce() {
-	return Ev::lift().then([this]() {
-		return fundpsbt();
-	}).then([this](Jsmn::Object res) {
-		if (!res.is_object())
-			return fail("fundpsbt did not return object", res);
-		if (!res.has("excess_msat"))
-			return fail("fundpsbt has no excess_msat", res);
+	auto no_onchain_funds = false;
+	try {
+		auto res = co_await fundpsbt();
+		if (!res.is_object()) {
+			co_await fail("fundpsbt did not return object", res);
+			co_return;
+		}
+		if (!res.has("excess_msat")) {
+			co_await fail("fundpsbt has no excess_msat", res);
+			co_return;
+		}
 		auto excess_msat = res["excess_msat"];
-		if (!Ln::Amount::valid_object(excess_msat))
-			return fail( "fundpsbt excess_msat not a valid amount"
-				   , excess_msat
-				   );
+		if (!Ln::Amount::valid_object(excess_msat)) {
+			co_await fail( "fundpsbt excess_msat not a valid amount"
+				     , excess_msat
+				     );
+			co_return;
+		}
 		auto amount = Ln::Amount::object(excess_msat);
 
-		return Boss::log( bus, Debug
-				, "OnchainFundsAnnouncer: "
-				  "Found %s (after deducting fee to spend) onchain."
-				, std::string(amount).c_str()
-				).then([this, amount]() {
-			return bus.raise(Msg::OnchainFunds{amount});
-		});
-	}).catching<RpcError>([this](RpcError const& e) {
-		return Boss::log( bus, Debug
-				, "OnchainFundsAnnouncer: "
-				  "No onchain funds found."
-				);
-	});
+		co_await Boss::log( bus, Debug
+				  , "OnchainFundsAnnouncer: "
+				    "Found %s (after deducting fee to spend) "
+				    "onchain."
+				  , std::string(amount).c_str()
+				  );
+		co_await bus.raise(Msg::OnchainFunds{amount});
+	} catch (RpcError const&) {
+		no_onchain_funds = true;
+	}
+	if (no_onchain_funds) {
+		co_await Boss::log( bus, Debug
+				  , "OnchainFundsAnnouncer: "
+				    "No onchain funds found."
+				  );
+	}
 }
 
 Ev::Io<Jsmn::Object>
 OnchainFundsAnnouncer::fundpsbt() {
-	return Ev::lift().then([this]() {
+	try {
 		/* On old C-Lightning, "reserve" is a bool.
 		 * Try that first.
 		 * If we get a parameter error -32602,
@@ -119,28 +128,28 @@ OnchainFundsAnnouncer::fundpsbt() {
 				.field("reserve", false)
 			.end_object()
 			;
-		return rpc->command("fundpsbt", std::move(params));
-	}).catching<RpcError>([this](RpcError const& e) {
+		co_return co_await rpc->command("fundpsbt", std::move(params));
+	} catch (RpcError const& e) {
 		/* If not a parameter error, throw.  */
 		if (int(double(e.error["code"])) != -32602) {
-			throw e;
+			throw;
 		}
-		/* Retry with "reserve" as a number.  */
-		auto params = Json::Out()
-			.start_object()
-				/* Get all the funds.  */
-				.field("satoshi", std::string("all"))
-				.field("feerate", std::string("normal"))
-				.field("startweight", (double) startweight)
-				.field("minconf", (double) minconf)
-				/* Do not reserve; we just want to know
-				 * how much money could be spent.
-				 */
-				.field("reserve", (double) 0)
-			.end_object()
-			;
-		return rpc->command("fundpsbt", std::move(params));
-	});
+	}
+	/* Retry with "reserve" as a number.  */
+	auto params = Json::Out()
+		.start_object()
+			/* Get all the funds.  */
+			.field("satoshi", std::string("all"))
+			.field("feerate", std::string("normal"))
+			.field("startweight", (double) startweight)
+			.field("minconf", (double) minconf)
+			/* Do not reserve; we just want to know
+			 * how much money could be spent.
+			 */
+			.field("reserve", (double) 0)
+		.end_object()
+		;
+	co_return co_await rpc->command("fundpsbt", std::move(params));
 }
 
 Ev::Io<void>
@@ -149,11 +158,12 @@ OnchainFundsAnnouncer::fail( std::string const& msg
 			   ) {
 	auto os = std::ostringstream();
 	os << res;
-	return Boss::log( bus, Error
-			, "OnchainFundsAnnouncer: %s: %s"
-			, msg.c_str()
-			, os.str().c_str()
-			);
+	co_await Boss::log( bus, Error
+			  , "OnchainFundsAnnouncer: %s: %s"
+			  , msg.c_str()
+			  , os.str().c_str()
+			  );
+	co_return;
 }
 
 OnchainFundsAnnouncer::~OnchainFundsAnnouncer() =default;
@@ -163,4 +173,3 @@ OnchainFundsAnnouncer::OnchainFundsAnnouncer(S::Bus& bus_)
 	{ start(); }
 
 }}
-
